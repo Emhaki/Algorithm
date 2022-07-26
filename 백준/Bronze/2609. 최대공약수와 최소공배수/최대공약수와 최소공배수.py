52f79803fbe7a17de29eb903b49c5274c7504e61
MyeongHak Lee <105331868+Emhaki@users.noreply.github.com>
2022-07-26 20:17:52 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 68 ms, Memory: 32952 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,24 +1,11 @@
-a, b = map(int, input().split())
+a,b = (input().split())
+a = int(a)
+b = int(b)
 
-a_result = []
-b_result = []
-c_result = []
+from math import gcd
 
-for a_least in range(1, 10001):
-    if a % a_least == 0:
-        a_result.append(a_least)
+GCD = gcd(a, b)
+LCM = a * b // GCD
 
-
-for b_least in range(1, 10001):
-    if b % b_least == 0:
-        b_result.append(b_least)
-
-# a_result = [1, 2, 3, 4, 6, 8, 12, 24]
-# b_result = [1, 2, 3, 6, 9, 18]
-for i in (a_result):
-    for j in (b_result):
-        if i == j:
-            c_result.append(i)
-
-print(max(c_result))
-print(a*b//max(c_result))
+print(GCD)
+print(LCM)
