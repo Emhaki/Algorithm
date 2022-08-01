addd008c0e82f478136bc1947912229202a6f843
MyeongHak Lee <105331868+Emhaki@users.noreply.github.com>
2022-08-01 17:05:29 +0900

    [Silver IV] Title: 괄호, Time: 88 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,20 +1,19 @@
 T = int(input())
 
+# (())())
 for i in range(T):
-    stack = []
-    input_word = input()
-    for j in input_word:
-        if '(' == j:
-            # stack 리스트에 쌓아준다.
-            stack.append(j)
-        elif ')' == j:
-            if stack:
-                stack.pop()
-            else:  # 스택에 괄호가 없을경우 NO
-                print("NO")
-                break
-    else:  # break로 끊기지 않고 수행됐을 경우
-        if not stack:  # 끊기지 않고 스택이 비어있다면 괄호가 맞은
-            print("YES")
-        else:  # 스택에 괄호가 들어있다면 NO
-            print("NO")
+    word = input()
+    word_list = list(word)
+    sum = 0
+    for i in word_list:
+        if i == '(':
+            sum += 1
+        elif i == ')':
+            sum -= 1
+        if sum < 0:
+            print('NO')
+            break
+    if sum > 0:
+        print('NO')
+    elif sum == 0:
+        print('YES')
