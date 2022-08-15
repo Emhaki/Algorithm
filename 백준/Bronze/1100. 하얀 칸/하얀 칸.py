d7dba1e270c433534c7edde4f7a782e0e442eeaa
MyeongHak Lee <105331868+Emhaki@users.noreply.github.com>
2022-08-15 09:53:18 +0900

    [Bronze II] Title: 하얀 칸, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -0,0 +1,8 @@
+arr = [list(map(str, input())) for _ in range(8)]
+cnt = 0
+for i in range(8):
+    for j in range(8):
+        if (i+j) % 2 == 0:
+            if arr[i][j] == 'F':
+                cnt += 1
+print(cnt)
