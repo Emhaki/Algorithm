7449de6f1380c1e6d10fd4629fde70e0562e5311
MyeongHak Lee <105331868+Emhaki@users.noreply.github.com>
2022-12-23 13:55:59 +0900

    [Bronze IV] Title: 상근날드, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.py
@@ -0,0 +1,10 @@
+sang = int(input())
+jung = int(input())
+ha = int(input())
+coke = int(input())
+splite = int(input())
+
+hamburger = [sang, jung, ha]
+beverage = [coke, splite]
+
+print(min(hamburger) + min(beverage) - 50)
