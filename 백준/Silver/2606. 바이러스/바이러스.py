8bb9fa7335b326b19eebedf8a44587d4239d7c82
MyeongHak Lee <105331868+Emhaki@users.noreply.github.com>
2022-12-23 11:12:16 +0900

    [Silver III] Title: 바이러스, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,35 +1,19 @@
-n = int(input())  # 정점 개수(컴퓨터)
-m = int(input())  # 간선 개수(네트워크)
-graph = [[] for _ in range(n+1)]
-total = 0
+N = int(input())
+M = int(input())
 
-# 인접 리스트 만들기
-for _ in range(m):
-    v1, v2 = map(int, input().split())
-    # 연결된 컴퓨터의 정보가 언제가 1부터 등장한다는 보장 x
-    graph[v1].append(v2)
-    graph[v2].append(v1)
+graph = [[] for _ in range(N+1)]
+visited = [0]*(N+1) # 사람이 읽기 쉽게 1, 2, 3으로
+# 그래프 생성
+for i in range(M):
+  a, b = map(int, input().split())
+  graph[a].append(b)
+  graph[b].append(a) # 양방향 연결
+# 함수 DFS
+def dfs(M): #연결선의 갯수
+  visited[M] = 1
+  for nx in graph[M]:
+    if visited[nx] == 0:
+      dfs(nx)
 
-# graph = [
-# []
-# [2,5],
-# [1,3,5]
-# [2],
-# [7],
-# [1,2,6]
-# [5],
-# [4]
-# ]
-
-
-def dfs(x, total):
-    visited[x] = True
-    for node in graph[x]:
-        if not visited[node]:
-            total = dfs(node, total + 1)
-    return total
-
-
-visited = [False for _ in range(n+1)]  # 방문 처리 리스트 만들기
-
-print(dfs(1, 0))
+dfs(1)
+print(sum(visited)-1)
