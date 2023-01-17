115979d4e4383aa6cc409366691e2559ccf6ee63
Myeonghak Lee <105331868+Emhaki@users.noreply.github.com>
2023-01-17 22:28:29 +0900

    [Silver II] Title: 스택 수열, Time: 4160 ms, Memory: 32212 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,29 @@
+count = 1
+result = True
+stack = []
+op = []
+
+N = int(input())
+for i in range(N):
+    num = int(input())
+    # num이하 숫자까지 스택에 넣기
+    while count <= num:
+        stack.append(count)
+        op.append('+')
+        count += 1
+
+    # num이랑 스택 맨 위 숫자가 동일하다면 제거
+    if stack[-1] == num:
+        stack.pop()
+        op.append('-')
+    # 스택 수열을 만들 수 없으므로 NO
+    else:
+        result = False
+        break
+
+# 스택 수열을 만들수 있는지 여부에 따라 출력 
+if result == False:
+    print("NO")
+else:
+    for i in op:
+        print(i)
