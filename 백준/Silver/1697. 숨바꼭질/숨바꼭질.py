71c5630940ed5d01df5765cca451b7353e67bf13
Myeonghak Lee <105331868+Emhaki@users.noreply.github.com>
2023-02-04 15:08:12 +0900

    [Silver I] Title: 숨바꼭질, Time: 120 ms, Memory: 34240 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,12 +1,16 @@
 import sys
 input = sys.stdin.readline
+
+# 수빈이 N
+# 동생 K
+
 from collections import deque
 N, K = map(int, input().split())
 MAX = 100001
 visited = [-1] * MAX
 visited[N] = 0 # 수빈이의 위치 방문처리
 queue = deque()
-queue.append((N,N))
+queue.append(N)
 
 # 수빈이가 더 큰 곳에 있다면 곱하기 연산과 더하기 연산은 불필요
 if N > K: # K = 5, N = 17
@@ -14,8 +18,7 @@ if N > K: # K = 5, N = 17
 
 else:
     while queue:
-        x, move = queue.popleft()
-
+        x = queue.popleft()
         if x == K:
             print(visited[x])
             break
@@ -23,4 +26,4 @@ else:
         for j in (x - 1), (x + 1), (x * 2):
             if 0 <= j < MAX and visited[j] == -1:
                 visited[j] = visited[x] + 1 # 시간 기록
-                queue.append((j, move + j))
+                queue.append(j)
