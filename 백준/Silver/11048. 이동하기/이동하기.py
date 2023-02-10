d64f1b8444aedc48c9c630e3148e0857656a36d5
Myeonghak Lee <105331868+Emhaki@users.noreply.github.com>
2023-02-10 09:57:55 +0900

    [Silver II] Title: 이동하기, Time: 764 ms, Memory: 78676 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -7,6 +7,6 @@ dp = [[0] * (M + 1) for _ in range(N+1)]
 
 for i in range(1, N + 1):
     for j in range(1, M + 1):
-        dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + candy[i-1][j-1]
+        dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + candy[i-1][j-1]
 
 print(dp[-1][-1])
