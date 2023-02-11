1361487382573991c80650fdea777939e0c877ab
Myeonghak Lee <105331868+Emhaki@users.noreply.github.com>
2023-02-11 14:55:42 +0900

    [Silver V] Title: 돌 게임, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+
+dp = [-1] * 10001
+# 1은 상근, 2는 창용 승
+dp[1] = 1
+dp[2] = 0
+dp[3] = 1
+
+for i in range(4, N+1):
+    if dp[i-1] == 1 or dp[i-3] == 1:
+        dp[i] = 0
+    else:
+        dp[i] = 1
+
+if dp[N] == 1:
+    print('SK')
+else:
+    print('CY')
