2070af7a7706dec2f405c0f2bbd7be93693ebfbf
Myeonghak Lee <105331868+Emhaki@users.noreply.github.com>
2023-02-22 16:24:47 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 948 ms, Memory: 72632 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+dp = [[0] * (n+1)]
+for _ in range(n):
+    num_list = [0] + list(map(int, input().split())) # 인덱스1을 1로 하기위해
+    dp.append(num_list)
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        dp[i][j] += dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+
+for _ in range(m):
+    x1,y1,x2,y2 = map(int, input().split())
+    print(dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1])
