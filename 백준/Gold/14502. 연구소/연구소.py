64718ce5e6ac3fe83aeb9131ee7b3736ae9323e2
Myeonghak Lee <105331868+Emhaki@users.noreply.github.com>
2023-03-04 08:36:19 +0900

    [Gold IV] Title: 연구소, Time: 2576 ms, Memory: 31720 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,49 +1,49 @@
 import sys
 input = sys.stdin.readline
-
+from itertools import combinations
 import copy
-def wall(wall_cnt):
-    # 벽 3개 설치시 함수실행
-    if wall_cnt == 3:
-        virus_start()
-        return
-    # 벽 설치 경우의 수 확인
-    for n in range(N):
-        for m in range(M):
-            if graph[n][m] == 0:
-                graph[n][m] = 1 # 벽 설치
-                wall(wall_cnt + 1)
-                graph[n][m] = 0 # dfs 실행 후 초기화
 
-def virus_start():
+def solve():
     global answer
-    # 기존 그래프 깊은 복사
-    wall = copy.deepcopy(graph)
-    virus = []
-    # 바이러스 위치 담기
-    for n in range(N):
-        for m in range(M):
-            if graph[n][m] == 2:
-                virus.append((n,m))
-    
-    while virus:
-        x, y = virus.pop()
-        for dx, dy in direction:
-            nx = x + dx
-            ny = y + dy
-            if 0 <= nx < N and 0 <= ny < M and wall[nx][ny] == 0:
-                wall[nx][ny] = 2
-                virus.append((nx, ny))
-    
-    safezone_cnt = 0
-    for row in wall:
-        safezone_cnt += row.count(0) # 안전지대 카운트
-    answer = max(answer, safezone_cnt)
 
-if __name__ == "__main__":
-    N, M = map(int, input().split())
-    graph = [list(map(int, input().split())) for _ in range(N)]
-    direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    answer = 0
-    wall(0)
-    print(answer)
+    for wall_combi in combinations(empty, wall_num):
+        graph_new = copy.deepcopy(graph)
+        for x, y in wall_combi:
+            graph_new[x][y] = 1
+        
+        # 바이러스 위치
+        virus = []
+        for n in range(N):
+            for m in range(M):
+                if graph_new[n][m] == 2:
+                    virus.append((n,m))
+        
+        while virus:
+            xv, yv = virus.pop()
+            
+            for i in range(4):
+                nx = dx[i] + xv
+                ny = dy[i] + yv
+
+                if 0 <= nx < N and 0 <= ny < M and graph_new[nx][ny] == 0:
+                    graph_new[nx][ny] = 2
+                    virus.append((nx,ny))
+        
+        safe_zone_cnt = 0
+        for row in graph_new:
+            safe_zone_cnt += row.count(0)
+        answer = max(answer, safe_zone_cnt)
+
+N, M = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+empty = []
+for n in range(N):
+    for m in range(M):
+        if graph[n][m] == 0:
+            empty.append((n,m))
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+answer = 0
+wall_num = 3
+solve()
+print(answer)
