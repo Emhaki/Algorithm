15d1913f14e621565838618a83238151435ef49c
Myeonghak Lee <105331868+Emhaki@users.noreply.github.com>
2023-04-01 10:32:42 +0900

    [Silver I] Title: 가장 큰 감소 부분 수열, Time: 112 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/17216. 가장 큰 감소 부분 수열/가장 큰 감소 부분 수열.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+dp = arr[:]
+
+for i in range(N):
+    for j in range(i):
+        if arr[j] > arr[i]:
+            dp[i] = max(dp[i], dp[j] + arr[i])
+print(max(dp))
