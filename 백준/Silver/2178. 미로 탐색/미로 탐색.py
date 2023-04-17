ae2a5e4f665c8699494a7a9ce53f230efa5e63fe
Myeonghak Lee <105331868+Emhaki@users.noreply.github.com>
2023-04-18 08:58:05 +0900

    [Silver I] Title: 미로 탐색, Time: 176 ms, Memory: 116568 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -2,25 +2,30 @@ import sys
 input = sys.stdin.readline
 
 from collections import deque
+# N, M 정수
 N, M = map(int, input().split())
-
 graph = [list(map(int, input().strip())) for _ in range(N)]
-visited = [[-1] * M for _ in range(N)]
-dx, dy = [0,0,-1,1], [-1,1,0,0]
+visited = [[0] * M for _ in range(N)]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 queue = deque()
-queue.append((0,0))
-visited[0][0] = 1
 
-while queue:
-    x, y = queue.popleft()
+def bfs(i,j):
+    queue.append((i,j))
+    
+    while queue:
+        x, y = queue.popleft()
 
-    for i in range(4):
-        nx, ny = dx[i] + x, dy[i] + y
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
 
-        if nx < 0 or nx >= N or ny < 0 or ny >= M:
-            continue
-        if graph[nx][ny] == 1 and visited[nx][ny] == -1:
-            visited[nx][ny] = visited[x][y] + 1
-            queue.append((nx,ny))
+            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1 and visited[nx][ny] == 0:
+                graph[nx][ny] = 0 # 지나간길 없애기
+                visited[nx][ny] = visited[x][y] + 1
+                queue.append((nx,ny))
+    
+    return visited[-1][-1]
 
-print(visited[N-1][M-1])
+
+print(bfs(0,0) + 1)
