c5d6a8528a3f518b238729b021649c2eb37820c7
Myeonghak Lee <105331868+Emhaki@users.noreply.github.com>
2023-04-21 13:51:42 +0900

    [Silver I] Title: 음식물 피하기, Time: 80 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -1,45 +1,49 @@
-from collections import deque
-# N 세로길이
-# M 가로길이
-# K 음식물 쓰레기 개수
-N, M, K = map(int, input().split())
-graph = [[]]
-for i in range(N+1):
-    graph.append([0] * (M+1))
-for i in range(K):
-    a, b = map(int, input().split())
-    graph[a][b] = 1
-#[
-# [], 
-# [1, 0, 0, 0], 
-# [0, 1, 1, 0], 
-# [1, 1, 0, 0]
-# ]
+import sys
+input = sys.stdin.readline
 
-# 4방향 탐색
+from collections import deque
+
+# N 세로, M 가로, K 개수
+N, M, K = map(int, input().split())
+
+graph = [['.'] * M for _ in range(N)]
+visited = [[False] * M for _ in range(N)]
+queue = deque()
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
 
-def bfs(i, j, graph):
-    q = deque([[i, j]])
-    graph[i][j] = 2
+# 음식물 그래프 삽입
+trash = []
+for i in range(K):
+    a, b = map(int, input().split())
+    trash.append((a,b))
+for x, y in trash:
+    graph[x-1][y-1] = '#'
+
+# bfs 생성
+def bfs(i,j):
+    visited[i][j] = True
+    queue.append((i,j))
     cnt = 1
 
-    while q:
-        x, y = q.popleft()
+    while queue:
+        x, y = queue.popleft()
 
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if 0 < nx <= N and 0 < ny <= M and graph[nx][ny] == 1: # 방향을 탐색하면서 범위 설정 및 쓰레기인지
-                q.append([nx, ny]) # 범위도 맞고 쓰레기도 맞다면 좌표 append
-                graph[nx][ny] = 2 # 방문처리
+            nx = dx[i] + x
+            ny = dy[i] + y
+
+            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == False and graph[nx][ny] == '#':
+                visited[nx][ny] = True
                 cnt += 1
+                queue.append((nx, ny))
+
     return cnt
 
-answer = 0
-for i in range(1, N+1):
-    for j in range(1, M+1):
-        if graph[i][j] == 1:
-            ans = bfs(i, j, graph)
-            answer = max(ans, answer)
-print(answer)
+result = []
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == '#':
+            result.append(bfs(i,j))
+
+print(max(result))
