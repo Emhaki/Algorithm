285128431eb18b9574968a06800a7c760ed98f28
Myeonghak Lee <105331868+Emhaki@users.noreply.github.com>
2023-07-29 15:44:10 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		     
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] str = br.readLine().split(" ");
+		int a = Integer.parseInt(str[0]);
+		int b = Integer.parseInt(str[1]);
+		
+		System.out.println(a-b);
+ 
+	}
+ 
+}
