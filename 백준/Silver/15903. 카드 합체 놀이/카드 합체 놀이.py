8fd619ed909a58cc81ad64ee1e78d88d636453c9
Myeonghak Lee <105331868+Emhaki@users.noreply.github.com>
2023-12-25 14:42:57 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 56 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -1,11 +1,12 @@
+from heapq import heappop, heapify, heappush
 N, M = map(int, input().split(" "))
 card_list = list(map(int, input().split(" ")))
 
+heapify(card_list)
 for _ in range(M):
-    idx = card_list.pop(card_list.index(min(card_list)))
-    idx2 = card_list.pop(card_list.index(min(card_list)))
-    new_number = idx + idx2
-    card_list.append(new_number)
-    card_list.append(new_number)
+    x = heappop(card_list)
+    y = heappop(card_list)
+    heappush(card_list, x+y)
+    heappush(card_list, x+y)
 
 print(sum(card_list))
